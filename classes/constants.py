0f887a06fe0b90af17abed3499a43d299c1b3e39
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-09-22 07:56:09 -0300

    added names and surnames

--- classes/constants.py
+++ classes/constants.py
@@ -1,6 +1,6 @@
-names= ["Rebeca","Ana","Catalina"]
+names = ['Isabella', 'Alice', 'Katherine', 'Eleanor', 'Margaret', 'Beatrice', 'Matilda', 'Agnes', 'Joan', 'Elizabeth', 'Emma', 'Constance', 'Philippa', 'Rosamund', 'Maud', 'Adelaide', 'Aelis', 'Alys', 'Amabel', 'Anabel', 'Adela', 'Agatha', 'Cecilia', 'Edith', 'Gertrude', 'Giselle', 'Juliana', 'Sybil', 'Theodora', 'Ursula', 'Ysabel.', 'Sarah', 'Ruth', 'Esther', 'Mary', 'Deborah', 'Elizabeth', 'Rachel', 'Leah', 'Rebecca', 'Miriam', 'Hannah', 'Abigail', 'Tamar', 'Bathsheba', 'Naomi', 'Jael', 'Jezebel', 'Delilah']
 
-surnames= ["The great", "Magnificent", "Milova", "Parka"]
+surnames = ['The Brave', 'The Wise', 'The Strong', 'The Just', 'The Mighty', 'The Valiant', 'The Bold', 'The Worthy', 'The Noble', 'The Fearless', 'The Loyal', 'The Courageous', 'The Honorable', 'The Skilled', 'The Respected', 'The Guardian', 'The Defender', 'The Chosen', 'The Leader', 'The Victorious', 'The Bold', 'The Brave', 'The Wise', 'The Black', 'The Strong', 'The Powerful', 'The Valiant', 'The Cunning', 'The Fearless', 'The Kind', 'The Noble', 'The Just', 'The Worthy', 'The Loyal', 'The Merciful', 'The Honorable', 'The Humble', 'The Gracious', 'The Magnificent', 'The Victorious', 'The Brave', 'The Wise', 'The Strong', 'The Just', 'The Valiant', 'The Noble', 'The Loyal', 'The Fearless', 'The Merciful', 'The Devout', 'The Faithful', 'The Chivalrous', 'The Honorable', 'The Courageous', 'The Generous', 'The Magnificent', 'The Resolute', 'The Victorious', 'The Revered', 'The Wise', 'The Heroic', 'The Gallant', 'The Worthy', 'The Mighty', 'The Majestic']
 
 stats=[
     "STR",
