8dc2843e28c7ba8ff9459be40e209a6666c6a58d
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-09-25 16:38:30 +0000

    added interactivity and functionality to chat model

--- classes/TODOchat.py
+++ classes/TODOchat.py
@@ -1,10 +1,10 @@
 import random
 preguntas= [
     {"pregunta":"Pregunta 1", "respuesta":{
-        "tipo a":["respuesta a 1","respuesta a 2"],
-        "tipo b":["respuesta b 1","respuesta b 2"],
-        "tipo c":["respuesta c 1","respuesta c 2"],
-        "tipo d":["respuesta d 1","respuesta d 2"]
+        "tipo a":["respuesta a 1 1","respuesta a 2 1"],
+        "tipo b":["respuesta b 1 1","respuesta b 2 1"],
+        "tipo c":["respuesta c 1 1","respuesta c 2 1"],
+        "tipo d":["respuesta d 1 1","respuesta d 2 1"]
     }},
      {"pregunta":"Pregunta 2", "respuesta":{
         "tipo a":["respuesta a 1","respuesta a 2"],
@@ -20,9 +20,17 @@ preguntas= [
       }}
     ]
 def chat(tipo):
-    for x in random.sample(preguntas,3):
-        print(x["pregunta"])
-        print(x["respuesta"][tipo])
+    preguntas_generadas=random.sample(preguntas,3)
+    for i,x in enumerate(preguntas_generadas):
+        print(f"{i+1}) {x['pregunta']}")
+    while True:
+        try:
+            selected = int(input("Choose one: "))
+            print(preguntas_generadas[selected-1]["respuesta"][tipo])
+            break
+        except:
+            print("Non valid option")
 
 
-chat("tipo a")
+
+chat("tipo b")
