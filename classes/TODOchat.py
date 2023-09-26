4818d0d2c3ab0899fadaecd932052f4bdabde6e1
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-09-26 16:47:28 +0000

    added interaction effect constant values

--- classes/TODOchat.py
+++ classes/TODOchat.py
@@ -1,8 +1,16 @@
 import random
-typos={
+TYPOS={
     "TIPO_A":"tipo a",
 }
-
+INTERACTION_EFFECT={
+    "POSITIVE":50,
+    "LITTLE_POSITIVE":25,
+    "OK": 5,
+    "IGNORED":0,
+    "DISLIKED":-5,
+    "BAD":-25,
+    "AWFUL":-50
+}
 preguntas= [
     {"pregunta":"Pregunta 1", "respuesta":{
         "tipo a":["respuesta a 1 1","respuesta a 2 1"],
@@ -10,11 +18,11 @@ preguntas= [
         "tipo c":["respuesta c 1 1","respuesta c 2 1"],
         "tipo d":["respuesta d 1 1","respuesta d 2 1"],
         "typos_effect":{
-            "tipo a": -20,
-            "tipo b": 30,
-            "tipo c": -20,
-            "tipo d": 30
-        }
+            "tipo a": INTERACTION_EFFECT["BAD"],
+            "tipo b": INTERACTION_EFFECT["BAD"],
+            "tipo c": INTERACTION_EFFECT["BAD"],
+            "tipo d": INTERACTION_EFFECT["BAD"]
+            }
         
     }},
      {"pregunta":"Pregunta 2", "respuesta":{
